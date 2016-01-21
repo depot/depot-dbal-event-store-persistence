3427c9408ec44b46ef052d02f8cfa2fb81717d4b
Alec Charlton <alec.charlton@monii.com>
2016-01-21 14:42:10 +0000

    Implement EventStoreManagement on Dbal

--- src/DbalPersistence.php
+++ src/DbalPersistence.php
@@ -5,6 +5,10 @@ namespace Depot\EventStore\Persistence\Adapter\Dbal;
 use DateTimeImmutable;
 use Depot\Contract\Contract;
 use Depot\Contract\ContractResolver;
+use Depot\EventStore\CommittedEventVisitor;
+use Depot\EventStore\Management\Criteria;
+use Depot\EventStore\Management\EventStoreManagement;
+use Depot\EventStore\Persistence\CommittedEvent;
 use Depot\EventStore\Persistence\OptimisticConcurrencyFailed;
 use Depot\EventStore\Transaction\CommitId;
 use Depot\EventStore\EventEnvelope;
@@ -13,7 +17,7 @@ use Depot\EventStore\Persistence\Persistence;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Connection;
 
-class DbalPersistence implements Persistence
+class DbalPersistence implements Persistence, EventStoreManagement
 {
     /**
      * @var Connection
@@ -101,17 +105,18 @@ class DbalPersistence implements Persistence
         $table->addColumn('committed_event_id', 'integer', ["unsigned" => true, "autoincrement" => true]);
         $table->addColumn('commit_id', $uuidType, $uuidParams);
         $table->addColumn('utc_committed_time', 'datetime');
-        $table->addColumn('aggregate_type', 'string', $stringParams);
-        $table->addColumn('aggregate_id', $uuidType, $uuidParams);
-        $table->addColumn('aggregate_version', 'integer');
+        $table->addColumn('aggregate_root_type', 'string', $stringParams);
+        $table->addColumn('aggregate_root_id', $uuidType, $uuidParams);
+        $table->addColumn('aggregate_root_version', 'integer');
         $table->addColumn('event_type', 'string', $stringParams);
         $table->addColumn('event_id', $uuidType, $uuidParams);
         $table->addColumn('event', 'text');
+        $table->addColumn('event_version', 'integer');
         $table->addColumn('when', 'datetime');
         $table->addColumn('metadata_type', 'string', array_merge($stringParams, ['notnull' => false]));
         $table->addColumn('metadata', 'text', ['notnull' => false]);
         $table->setPrimaryKey(['committed_event_id']);
-        $table->addIndex(['aggregate_type', 'aggregate_id', 'aggregate_version']);
+        $table->addIndex(['aggregate_root_type', 'aggregate_root_id', 'aggregate_root_version']);
 
         return $table;
     }
@@ -141,12 +146,10 @@ class DbalPersistence implements Persistence
             ;
 
             $eventEnvelopes[] = new EventEnvelope(
-                $aggregateRootType,
-                $aggregateRootId,
                 $eventType,
                 $record['event_id'],
                 $this->eventSerializer->deserialize($eventType, $event),
-                $record['aggregate_version'],
+                (int) $record['event_version'],
                 new DateTimeImmutable($record['when']),
                 $metadataType,
                 $metadata
@@ -162,13 +165,15 @@ class DbalPersistence implements Persistence
      * @param string $aggregateRootId
      * @param int $expectedAggregateRootVersion
      * @param EventEnvelope[] $eventEnvelopes
+     * @param DateTimeImmutable|null $now
      */
     public function commit(
         CommitId $commitId,
         Contract $aggregateRootType,
         $aggregateRootId,
         $expectedAggregateRootVersion,
-        array $eventEnvelopes
+        array $eventEnvelopes,
+        $now = null
     ) {
         $aggregateRootVersion = $this->versionFor($aggregateRootType, $aggregateRootId);
 
@@ -176,7 +181,9 @@ class DbalPersistence implements Persistence
             throw new OptimisticConcurrencyFailed();
         }
 
-        $utcCommittedTime = new \DateTimeImmutable('now');
+        if (! $now) {
+            $now = new \DateTimeImmutable('now');
+        }
 
         foreach ($eventEnvelopes as $eventEnvelope) {
             $metadata = $eventEnvelope->getMetadataType()
@@ -190,10 +197,10 @@ class DbalPersistence implements Persistence
             ;
             $values = [
                 'commit_id' => $commitId,
-                'utc_committed_time' => $utcCommittedTime->format('Y-m-d H:i:s'),
-                'aggregate_type' => $aggregateRootType->getContractName(),
-                'aggregate_id' => $aggregateRootId,
-                'aggregate_version' => ++$aggregateRootVersion,
+                'utc_committed_time' => $now->format('Y-m-d H:i:s'),
+                'aggregate_root_type' => $aggregateRootType->getContractName(),
+                'aggregate_root_id' => $aggregateRootId,
+                'aggregate_root_version' => $aggregateRootVersion,
                 'event_type' => $eventEnvelope->getEventType()->getContractName(),
                 'event_id' => $eventEnvelope->getEventId(),
                 'event' => json_encode(
@@ -202,6 +209,7 @@ class DbalPersistence implements Persistence
                         $eventEnvelope->getEvent()
                     )
                 ),
+                'event_version' => $eventEnvelope->getVersion(),
                 '`when`' => $eventEnvelope->getWhen()->format('Y-m-d H:i:s'),
                 'metadata_type' => $eventEnvelope->getMetadataType()
                     ? $eventEnvelope->getMetadataType()->getContractName()
@@ -213,11 +221,12 @@ class DbalPersistence implements Persistence
         }
     }
 
-    private function findByaggregateRootTypeAndId($aggregateRootType, $aggregateRootId)
+    private function findByAggregateRootTypeAndId($aggregateRootType, $aggregateRootId)
     {
         $query = "SELECT * FROM "
             .$this->tableName.
-            " WHERE aggregate_type = :aggregateRootType AND aggregate_id = :aggregateRootId ORDER BY aggregate_version";
+            " WHERE aggregate_root_type = :aggregateRootType
+            AND aggregate_root_id = :aggregateRootId ORDER BY aggregate_root_version";
         $statement = $this->connection->prepare($query);
         $statement->bindValue('aggregateRootType', $aggregateRootType->getContractName());
         $statement->bindValue('aggregateRootId', $aggregateRootId);
@@ -233,11 +242,97 @@ class DbalPersistence implements Persistence
         $result = $this->findByaggregateRootTypeAndId($aggregateRootType, $aggregateRootId);
 
         while ($row = $result->fetch()) {
-            if ($row['aggregate_version'] > $version) {
-                $version = $row['aggregate_version'];
+            if ($row['event_version'] > $version) {
+                $version = $row['event_version'];
             }
         }
 
         return (int) $version;
+    }
+
+    private function deserializeCommittedEvent($row)
+    {
+        return new CommittedEvent(
+            CommitId::fromString($row['commit_id']),
+            DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $row['utc_committed_time']),
+            new Contract($row['aggregate_root_type'], str_replace('.','\\',$row['aggregate_root_type'])),
+            $row['aggregate_root_id'],
+            (int) $row['aggregate_root_version'],
+            new EventEnvelope(
+                $this->eventContractResolver->resolveFromContractName($row['event_type']),
+                $row['event_id'],
+                $this->eventSerializer->deserialize(
+                    $this->eventContractResolver->resolveFromContractName($row['event_type']),
+                    json_decode($row['event'], true)
+                ),
+                (int) $row['event_version'],
+                DateTimeImmutable::createFromFormat('Y-m-d H:i:s', $row['when'])
+            )
+        );
+    }
+
+    public function visitCommittedEvents(Criteria $criteria, CommittedEventVisitor $committedEventVisitor)
+    {
+        $statement = $this->prepareVisitCommittedEventsStatement($criteria);
+        $statement->execute();
+
+        while ($row = $statement->fetch()) {
+            $committedEvent = $this->deserializeCommittedEvent($row);
+
+            $committedEventVisitor->doWithCommittedEvent($committedEvent);
+        }
+    }
+
+    private function prepareVisitCommittedEventsStatement(Criteria $criteria)
+    {
+        list ($where, $bindValues, $bindValueTypes) = $this->prepareVisitCommittedEventsStatementWhereAndBindValues($criteria);
+        $query = 'SELECT *
+            FROM ' . $this->tableName . '
+            ' . $where . '
+            ORDER BY committed_event_id ASC';
+
+        $statement = $this->connection->executeQuery($query, $bindValues, $bindValueTypes);
+
+        return $statement;
+    }
+
+    private function prepareVisitCommittedEventsStatementWhereAndBindValues(Criteria $criteria)
+    {
+        $bindValues = [];
+        $bindValueTypes = [];
+
+        $criteriaTypes = [];
+
+        if ($criteria->getAggregateRootTypes()) {
+            $criteriaTypes[] = 'aggregate_root_type IN (:aggregateRootTypes)';
+            $aggregateRootTypeContractNames = array_map( function (Contract $aggregateRootType){
+                return $aggregateRootType->getContractName();
+            }, $criteria->getAggregateRootTypes());
+            $bindValues['aggregateRootTypes'] = $aggregateRootTypeContractNames;
+            $bindValueTypes['aggregateRootTypes'] = Connection::PARAM_STR_ARRAY;
+        }
+
+        if ($criteria->getAggregateRootIds()) {
+            $criteriaTypes[] = 'aggregate_root_id IN (:aggregateRootIds)';
+            $bindValues['aggregateRootIds'] = $criteria->getAggregateRootIds();
+            $bindValueTypes['aggregateRootIds'] = Connection::PARAM_STR_ARRAY;
+        }
+
+        if ($criteria->getEventTypes()) {
+            $criteriaTypes[] = 'event_type IN (:eventTypes)';
+            $eventTypeContractNames = array_map( function (Contract $eventType){
+                return $eventType->getContractName();
+            }, $criteria->getEventTypes());
+            $bindValues['eventTypes'] = $eventTypeContractNames;
+            $bindValueTypes['eventTypes'] = Connection::PARAM_STR_ARRAY;
+        }
+
+        if (! $criteriaTypes) {
+            return array('', [], []);
+        }
+
+        $where = 'WHERE '.join(' AND ', $criteriaTypes);
+
+        return array($where, $bindValues, $bindValueTypes);
     }
 }
---
+++ tests/integration/DbalEventStoreManagementTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Depot\Testing\Integration\EventStore\Persistence\Adapter\Dbal;
+
+use Depot\Testing\EventStore\Persistence\EventStoreManagementTest;
+use Doctrine\DBAL\Portability\Connection;
+use Doctrine\DBAL\DriverManager;
+use Depot\Contract\SimplePhpFqcnContractResolver;
+use Depot\EventStore\Persistence\Adapter\Dbal\DbalPersistence;
+use Depot\EventStore\Serialization\Adapter\MoniiReflectionPropertiesSerializer\MoniiReflectionPropertiesSerializer;
+
+class DbalEventStoreManagementTest extends EventStoreManagementTest
+{
+    private $dbalPersistence;
+
+    protected function createPersistence()
+    {
+        $serializer = new MoniiReflectionPropertiesSerializer(
+            new SimplePhpFqcnContractResolver()
+        );
+
+        $contractResolver = new SimplePhpFqcnContractResolver();
+
+        $connection = $this->getConnection();
+
+        $schemaManager = $connection->getSchemaManager();
+        $schema = $schemaManager->createSchema();
+
+        $this->dbalPersistence = new DbalPersistence(
+            $connection,
+            $serializer,
+            $serializer,
+            $contractResolver,
+            $contractResolver,
+            'event'
+        );
+        $table = $this->dbalPersistence->configureSchema($schema);
+
+        if ($table) {
+            $schemaManager->createTable($table);
+        }
+
+        return $this->dbalPersistence;
+    }
+
+    protected function getPersistence()
+    {
+        return $this->dbalPersistence;
+    }
+
+    /**
+     * @return Connection
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function getConnection()
+    {
+        return  DriverManager::getConnection([
+            'driver' => 'pdo_sqlite',
+            'memory' => true
+        ]);
+    }
+}
